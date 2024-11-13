035ee7ed7c06d0809df2b5b88095c337dadcdc30
fadouabenjedda <fadouabenjedda01@gmail.com>
2024-11-13 15:19:57 +0100

    stylisation css

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById(`contact_form`).addEventListener(`submit`,function(event){
+    event.preventDefault();
+    const nom = document.getElementById(`form_nom`).value;
+    const email = document.getElementById(`form_email`).value;
+    const message = document.getElementById(`form_email`).value;
+
+})
