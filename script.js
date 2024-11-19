f42465fe560d0440382bbdf9977a757299922690
fadouabenjedda <fadouabenjedda01@gmail.com>
2024-11-19 10:43:47 +0100

    commit

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+emailjs.init("-XVxAGhylANCibEBM");
 document.getElementById(`contact_form`).addEventListener(`submit`,function(event){
+
     event.preventDefault();
+
     const nom = document.getElementById(`form_nom`).value;
     const email = document.getElementById(`form_email`).value;
-    const message = document.getElementById(`form_email`).value;
+    const message = document.getElementById(`form_message`).value;
 
-})
+    if(!nom || !email || !message){
+        alert("Veuillez remplir tous les champs !");
+        return;
+    }
+
+    const params = {
+        nom : nom,
+        email : email,
+        message: message,
+    };
+
+
+    emailjs.send("service_r3wx7qf","template_7pcaj3n",params)
+   
+
+    .then(function(){
+        alert("Message envoyé avec succsès !");
+        document.getElementById(`contact_form`).reset();
+    })
+
+    .catch(function(error) {
+        alert("Une erreur est survenue.");
+    });
+});
+
+    
+
+
+
