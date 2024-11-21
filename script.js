53308c7df71b1b0dfe5cb39e12f27360707708ac
fadouabenjedda <fadouabenjedda01@gmail.com>
2024-11-21 11:42:34 +0100

    next commit

--- script.js
+++ script.js
@@ -1,36 +1,55 @@
-emailjs.init("-XVxAGhylANCibEBM");
-document.getElementById(`contact_form`).addEventListener(`submit`,function(event){
+const burgerIcon = document.querySelector(`.menu_burger`);
+const menu = document.querySelector(`.menu`);
 
-    event.preventDefault();
-
-    const nom = document.getElementById(`form_nom`).value;
-    const email = document.getElementById(`form_email`).value;
-    const message = document.getElementById(`form_message`).value;
-
-    if(!nom || !email || !message){
-        alert("Veuillez remplir tous les champs !");
-        return;
-    }
-
-    const params = {
-        nom : nom,
-        email : email,
-        message: message,
-    };
+burgerIcon.addEventListener(`click`, ()=>{
+    menu.classList.toggle(`active`);
+});
 
 
-    emailjs.send("service_r3wx7qf","template_7pcaj3n",params)
+
+
+
+
+
+
+
+
+
+
+
+// emailjs.init("-XVxAGhylANCibEBM");
+// document.getElementById(`contact_form`).addEventListener(`submit`,function(event){
+
+//     event.preventDefault();
+
+//     const nom = document.getElementById(`form_nom`).value;
+//     const email = document.getElementById(`form_email`).value;
+//     const message = document.getElementById(`form_message`).value;
+
+//     if(!nom || !email || !message){
+//         alert("Veuillez remplir tous les champs !");
+//         return;
+//     }
+
+//     const params = {
+//         nom : nom,
+//         email : email,
+//         message: message,
+//     };
+
+
+//     emailjs.send("service_r3wx7qf","template_7pcaj3n",params)
    
 
-    .then(function(){
-        alert("Message envoyé avec succsès !");
-        document.getElementById(`contact_form`).reset();
-    })
+//     .then(function(){
+//         alert("Message envoyé avec succsès !");
+//         document.getElementById(`contact_form`).reset();
+//     })
 
-    .catch(function(error) {
-        alert("Une erreur est survenue.");
-    });
-});
+//     .catch(function(error) {
+//         alert("Une erreur est survenue.");
+//     });
+// });
 
     
 
