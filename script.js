d609be64509896ec2ae959e7ba7d51dff38e98b4
fadouabenjedda <fadouabenjedda01@gmail.com>
2024-11-27 15:53:18 +0100

    next commit

--- script.js
+++ script.js
@@ -18,6 +18,30 @@ document.addEventListener(`scroll`,function(){
 })
 
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     const hiddenElements = document.querySelectorAll('.hidden');
+
+//     // Fonction pour vérifier si l'élément est visible dans la fenêtre
+//     const isVisible = (element) => {
+//         const rect = element.getBoundingClientRect();
+//         return rect.top < window.innerHeight && rect.bottom > 0;
+//     };
+
+//     // Fonction pour ajouter la classe "show" aux éléments visibles
+//     const checkVisibility = () => {
+//         hiddenElements.forEach(element => {
+//             if (isVisible(element)) {
+//                 element.classList.add('show');
+//             }
+//         });
+//     };
+
+//     // Exécuter la vérification lors du scroll
+//     window.addEventListener('scroll', checkVisibility);
+
+//     // Vérification initiale au chargement de la page
+//     checkVisibility();
+// });
 
 
 
