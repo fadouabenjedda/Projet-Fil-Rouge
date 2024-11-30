995d1e62fb957fa0398c75121a819b0dbf5bf04f
fadouabenjedda <fadouabenjedda01@gmail.com>
2024-11-30 10:16:02 +0100

    NEXT COMMIT

--- script.js
+++ script.js
@@ -7,43 +7,6 @@ burgerIcon.addEventListener(`click`, ()=>{
 
 
 
-document.addEventListener(`scroll`,function(){
-    const elements = document.querySelectorAll(`.slid_in_up`);
-    const scrollTop = window.scrollY + window.innerHeight;
-    elements.forEach((elements)=>{
-        if (element.offsetTop < scrollTop){
-            element.classList.add(`active`);
-        }
-    })
-})
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const hiddenElements = document.querySelectorAll('.hidden');
-
-//     // Fonction pour vérifier si l'élément est visible dans la fenêtre
-//     const isVisible = (element) => {
-//         const rect = element.getBoundingClientRect();
-//         return rect.top < window.innerHeight && rect.bottom > 0;
-//     };
-
-//     // Fonction pour ajouter la classe "show" aux éléments visibles
-//     const checkVisibility = () => {
-//         hiddenElements.forEach(element => {
-//             if (isVisible(element)) {
-//                 element.classList.add('show');
-//             }
-//         });
-//     };
-
-//     // Exécuter la vérification lors du scroll
-//     window.addEventListener('scroll', checkVisibility);
-
-//     // Vérification initiale au chargement de la page
-//     checkVisibility();
-// });
-
-
 
 
 
