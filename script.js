4434f69c96c7435abe956546306e69c9a72b437f
fadouabenjedda <fadouabenjedda01@gmail.com>
2024-12-02 17:30:27 +0100

    next commit

--- script.js
+++ script.js
@@ -31,30 +31,6 @@ let articles = [
   
 
 
-// // Tableau pour stocker les articles
-// let articles = [
-//     { id: 1, title: "Figma", content: "Figma est un outil de design collaboratif très populaire pour créer des interfaces utilisateur et des prototypes. Accessible depuis un navigateur, il offre des fonctionnalités puissantes pour les débutants comme pour les experts." },
-//     { id: 2, title: "GitHub", content: "GitHub est une plateforme incontournable pour les développeurs. Elle permet de gérer des projets, collaborer avec des équipes et versionner du code." }
-//     { id: 3, title: "Javascript" , content: "Grâce à JavaScript, vous pouvez transformer une page statique en une expérience utilisateur fluide et engageante. Ce langage est également compatible avec des frameworks modernes comme React, Vue.js, et Angular."}
-//   ];
-  
-// //   Fonction pour afficher les articles
-//   function afficherArticles() {
-//     const articlesDiv = document.getElementById("articles");
-//     articlesDiv.innerHTML = "";
-  
-//     articles.forEach(article => {
-//       const articleDiv = document.createElement("div");
-//       articleDiv.className = "article";
-//       articleDiv.innerHTML = ` <h3>${article.title}</h3> <p>${article.content}</p>`;
-//       articlesDiv.appendChild(articleDiv);
-//     });
-//   }
-  
-//   afficherArticles();
-
-
-
 
 
 
